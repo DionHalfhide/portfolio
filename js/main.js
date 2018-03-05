90efa6f598d5a33f12219bc62be8e5cc955811d1
Dion Halfhide <1035307@idcollege.nl>
2018-03-05 11:40:24 +0100

    start van de mediaq
    alles op mijn mac staat goed site bijna klaar

--- js/main.js
+++ js/main.js
@@ -29,27 +29,28 @@ $(document).ready(function () {
 
 $(window).scroll(function () {
     var Beneden = 0;
-    var Rechts = 0;
-
-    if ($(document).scrollTop() > 600) {
-        Beneden = Beneden - 75;
-        document.getElementById("shit").style.marginTop = Beneden + "vh";
-        console.log(document.getElementById("shit").style.marginTop);
-        document.getElementById("home").innerHTML = 'HOME';
     
-    } else if ($(document).scrollTop() < 100 && document.getElementById("shit").style.marginTop == '-75vh') {
+    if ($(document).scrollTop() > 600) {
+        Beneden = Beneden - 77;
+        document.getElementById("shit").style.marginTop = Beneden + "vh";
+
+        document.getElementById("home").innerHTML = 'HOME';
+
+    } else if ($(document).scrollTop() < 100 && document.getElementById("shit").style.marginTop == '-77vh') {
         Beneden = Beneden + 0;
         document.getElementById("shit").style.marginTop = Beneden + "%";
+
         document.getElementById("home").innerHTML = '';
         document.getElementById("home").style.borderRight = 'none';
     }
 });
+
 //zorgt er voor dat border right bij home niet komt en gaat bij herladen----
 $(window).ready(function () {
-    if ($(document).click() ) {
-       document.getElementById("home").style.borderRight = 'none';
-    
-    } else if ($(document).click() ){
+    if ($(document).click()) {
+        document.getElementById("home").style.borderRight = 'none';
+
+    } else if ($(document).click()) {
         document.getElementById("home").style.borderRight = 'none';
     }
 });
@@ -62,55 +63,7 @@ $(document).click(function (event) {
         (document.getElementById('home').style.borderRight = '1px solid orange');
     }
 });
-$(document).click(function (event) {
-    if ($(event.target).is('#face')) {
-        (document.getElementById('face').innerHTML = '');
-    } else {
-        (document.getElementById('home').style.borderRight = '1px solid orange');
-    }
-});
-$(document).ready(function () {
-    // When the user scrolls down 20px from the top of the document, show the button
-    window.onscroll = function () {
-        scrollFunction()
-    };
 
-    function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            document.getElementById("myBtn").style.display = "block";
-        } else {
-            document.getElementById("myBtn").style.display = "none";
-        }
-    }
-
-    // When the user clicks on the button, scroll to the top of the document
-    function topFunction() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-    }
-
-    function doeIets() {
-        alert("Thank you for choosing AZTEC CASITA catering service ");
-    }
-    $(function () {
-        $("#button").click(function () {
-            $("#button").addClass("onclic", 250, validate);
-        });
-
-        function validate() {
-            setTimeout(function () {
-                $("#button").removeClass("onclic");
-                $("#button").addClass("validate", 450, callback);
-            }, 2250);
-        }
-
-        function callback() {
-            setTimeout(function () {
-                $("#button").removeClass("validate");
-            }, 1250);
-        }
-    })
-});
 
 $(document).ready(function () {
     $('.item').bind('click', function () {
@@ -120,29 +73,3 @@ $(document).ready(function () {
         $(this).addClass('active');
     });
 });
-/*
-$(document).click(function (event) {
-    if ($(event.target).is('#home')) {
-        (document.getElementById('home').style.borderRightColor = 'none');
-        (document.getElementById('a').style.borderRightColor = 'orange');
-        (document.getElementById('b').style.borderRightColor = 'orange');
-        (document.getElementById('c').style.borderRightColor = ' orange');
-    } else if ($(event.target).is('#a')) {
-        (document.getElementById('home').style.borderRightColor = 'red');
-        (document.getElementById('a').style.borderRightColor = 'red');
-        (document.getElementById('b').style.borderRightColor = 'red');
-        (document.getElementById('c').style.borderRightColor = ' red');
-    } else if ($(event.target).is('#b')) {
-        (document.getElementById('home').style.borderRightColor = 'orange');
-        (document.getElementById('a').style.borderRightColor = 'orange');
-        (document.getElementById('b').style.borderRightColor = 'orange');
-        (document.getElementById('c').style.borderRightColor = ' orange');
-    } else if ($(event.target).is('#c')) {
-         (document.getElementById('home').style.borderRightColor = 'red');
-        (document.getElementById('a').style.borderRightColor = 'red');
-        (document.getElementById('b').style.borderRightColor = 'red');
-        (document.getElementById('c').style.borderRightColor = ' red');
-    } else {
-        alert('bullshit');
-    }
-});*/
